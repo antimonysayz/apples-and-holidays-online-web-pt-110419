cc5a476a283f6cbade68fb932f9dd732f3a1a034
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 02:43:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.captialize}" 
-    
+    holiday.each do |
 end
 
 def all_holidays_with_bbq(holiday_hash)
